558d312012aa099c95262886cd27baf1043358e7
SurajChadha001 <surajchadhamca@gmail.com>
2025-01-10 18:25:59 +0530

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Logo from "../assets/logo.svg"
+import "./header.css";
 const Header = () => {
   return (
     <div>
-      <img className="logo" src={Logo} alt="" />
+      <img styleclassName="logo" src={Logo} alt="" />
     </div>
   )
 }
