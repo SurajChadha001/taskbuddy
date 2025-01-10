6b7e86d52c2407ad04dc5cf3f070fb7e9a78d93f
SurajChadha001 <surajchadhamca@gmail.com>
2025-01-10 18:02:01 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./components/Header";
+import Footer from './components/Footer';
+function App() {
+  return (
+    <>
+    <Header />
+    <div className='active'>
+     <h1>Suraj</h1>
+    </div>
+    <Footer/>
+    </>
+    
+  )
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Logo from "../assets/logo.svg"
+const Header = () => {
+  return (
+    <div>
+      <img className="logo" src={Logo} alt="" />
+    </div>
+  )
+}
+
+export default Header
